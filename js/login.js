1abbe1287fe9338f28f7d290c3659557ad81c80b
lucilleanne <lucilleanneteves@gmail.com>
2015-04-17 17:30:18 +0800

    Update

--- js/login.js
+++ js/login.js
@@ -2,20 +2,10 @@ $(function() {
     Parse.$ = jQuery;
     // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("buWMcHVkpXrUcQ8LlyY8t4nTQxaHLY10w97A09bH", "2hhbkfu8YDfOexCcY7g79rEg0QCvv0uNm1b6dl2q"); // p-clearance-001
-	/*
-	Parse.Cloud.run('login', {
-			username: "Glenn",
-			password: "Manghi"
-	}, 
-	{
-		success: function(result) {
-			// result is 'Hello world!'
-		},
-		error: function(error) {
-		
-		}
-	});
-	*/
+	current = Parse.User.current();
+	// if(current){
+		// window.open('step1.html',"_self");
+	// }
 });
 
 $('.form-signin').on('submit', function(e) {
@@ -27,18 +17,25 @@ $('.form-signin').on('submit', function(e) {
 	username = data[0].value;
 	password = data[1].value;
 	
-	Parse.Cloud.run('login', {
-		username: username,
-		password: password
-	}, 
-	{
-		success: function(result) {
-			alert('Welcome!');
-			window.open("step1.html","_self");
-		},
-		error: function(error) {
-			console.log(error);
-			alert(error.message);
+	Parse.User.logIn(username,password,function(response){
+		console.log(response);
+		if(response){
+			window.open('step1.html',"_self");
 		}
 	});
+	
+	// Parse.Cloud.run('login', {
+		// username: username,
+		// password: password
+	// }, 
+	// {
+		// success: function(result) {
+			// alert('Welcome!');
+			// // window.open("step1.html","_self");
+		// },
+		// error: function(error) {
+			// console.log(error);
+			// alert(error.message);
+		// }
+	// });
 });
